a4f2b619a59bd7f78a7b8e715ee46d80ef109387
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-02-25 21:23:27 +0200

    push=2/5

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppWrapper from './App';
+import App from './App';
 import './index.css';
+
+// Імпортуйте Provider
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppWrapper />
+    {}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
 
-// Налаштування persist для контактів
-const contactsPersistConfig = {
-  key: 'contacts',
-  storage,
-};
-
-const persistedContactsReducer = persistReducer(contactsPersistConfig, contactsReducer);
-
 const store = configureStore({
   reducer: {
-    contacts: persistedContactsReducer,
+    contacts: contactsReducer,
     filters: filtersReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
+      serializableCheck: false,
     }),
 });
 
-const persistor = persistStore(store);
-
-export { store, persistor };
+export default store;
